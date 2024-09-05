ebf0d7d36b52b0fabcb69f01dbc17ac404dab41b
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-09-05 16:14:25 +0000

    Implements the multiplyBy operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { from, of } from 'rxjs';
+import { from, Observable, of } from 'rxjs';
 import { multiplyBy } from './operators/custom-operators';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass',
 })
 export class AppComponent {
   title = 'rxjs-operator';
-  numbersArr = [1, 2, 3, 'star', 'fruit'];
-  source$ = of(...this.numbersArr);
+  source$: Observable<number | string> = from([1, 2, 3, 'star', 'fruit']);
+  data: (number | string)[] = [];
 
   ngOnInit() {
     this.source$.pipe(multiplyBy(3)).subscribe({
-      next: (value) => console.log(value),
+      next: (value) => this.data.push(value),
       error: (error) => console.log(error),
       complete: () => console.log('Completed'),
     });
